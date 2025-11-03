5ccd4c0cc5f0ec5ec263ccab5e6b38ed6d627aaa
Subrahmanya Bhat <subgb98@gmail.com>
2025-11-03 23:14:44 +0530

    Upgraded to angular 17

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { settings } from 'src/assets/appsettings';
 import { ApiResponseModel } from '../interfaces/api-response-model';
 import { LoginModel, RegisterModel, UserModel } from '../interfaces/user';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
