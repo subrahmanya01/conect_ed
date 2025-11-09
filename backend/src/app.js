4c065579b8d41508e45596bddbfd3a3ff4de31bf
Subrahmanya Bhat <subgb98@gmail.com>
2025-11-09 13:34:01 +0530

    Corrected vercel config.

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const io= new Server(server)
 
 connectSocket(io);
 
-server.listen(port, err => {
+server.listen(process.env.PORT || port, err => {
   if (err) {
     console.log(err);
     return process.exit(1);
